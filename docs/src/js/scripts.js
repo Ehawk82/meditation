1bd29a2cc93e2107ec1c47b3fd4e5dc96f182fe5
Jason Graziano <mrehawk82@gmail.com>
2022-04-24 14:50:34 -0400

    touch ups
    without git bash on this machine, i'm playing golf

--- docs/src/js/scripts.js
+++ docs/src/js/scripts.js
@@ -26,7 +26,11 @@ contFunc = (body) => {
     for (let r = 0; r < 10; r++) {
         var row = createEle("tr");
         for(let d = 0; d < 10; d++) {
-            row.innerHTML = "r:"+r+"d"+d;
+            var td = createEle("td");
+          
+            td.innerHTML = "r:" + r + "; d: " + d;
+          
+            row.append(td)
         }
     }
     table.className = "table w3-grey w3-card-4";
