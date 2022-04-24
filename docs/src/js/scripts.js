d67115d3b66002f0964c13757383c8c4e87845dd
Jason Graziano <mrehawk82@gmail.com>
2022-04-24 11:41:58 -0400

    table and rows
    busy week; things have been added.

--- docs/src/js/scripts.js
+++ docs/src/js/scripts.js
@@ -3,11 +3,34 @@ const init = () => {
 },
 buildApp = () => {
   var body = byId("body"),
-      welcomePage = createEle("div");
+      welcomePage = createEle("div"),
+      welcomeMessage = createEle("div"),
+      contBtn = createEle("button");
   
-  welcomePage.innerHTML = "Welcome to the meditation app!";
-  console.log("hello JS");
+  contBtn.innerHTML = "CONTINUE";
+  contBtn.onclick = contFunc(body);
+  
+  welcomeMessage.innerHTML = "Welcome to the meditation app";
+  
+  welcomePage.append(welcomeMessage,contBtn);
+
   body.append(welcomePage);
+},
+contFunc = (body) => {
+  return () => {
+    body.innerHTML = "";
+    
+    var assessmentChart = createEle("div"),
+        table = createEle("table");
+    
+    for (let r = 0; r < 10; r++) {
+        var row = createEle("tr");
+      
+        row.innerHTML = r;
+    }
+    
+    body.append(assessmentChart);
+  }
 };
 window.onload = () => {
   init();
